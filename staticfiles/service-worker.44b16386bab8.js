624942b6ad501f6f2847139b1381354ea09c54f1
Alexandre Alvarado <alexander3003alvarado@gmail.com>
2025-12-10 18:33:18 -0300

    serve static with whitenoise

---
+++ staticfiles/service-worker.44b16386bab8.js
@@ -0,0 +1,41 @@
+// Service Worker para SmashPoint
+
+const CACHE_NAME = 'smashpoint-v1';
+const urlsToCache = [
+  '/',
+  '/static/css/styles.css',
+  '/index/'
+];
+
+// Instalar el service worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Activar el service worker
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => {
+      return resp || fetch(event.request).catch(() => caches.match('/'));
+    })
+  );
+});
