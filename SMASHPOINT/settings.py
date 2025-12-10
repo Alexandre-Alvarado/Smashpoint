e851564a196804ad0a4bd9d67280913355429d17
Alexandre Alvarado <alexander3003alvarado@gmail.com>
2025-12-10 18:00:03 -0300

    switch to sqlite for render

--- SMASHPOINT/settings.py
+++ SMASHPOINT/settings.py
@@ -83,14 +83,9 @@ WSGI_APPLICATION = 'SMASHPOINT.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': { 
-        'ENGINE': 'django.db.backends.mysql', 
-        'NAME': 'BD_smashpoint', 
-        'USER': 'root', 
-        'PASSWORD': '', 
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-        }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
